b94a992a818ca7762204f6d8bbfef4b8dd611102
Unknown <ljw041101@gmail.com>
2020-10-27 23:26:37 +0900

    2020 10 27

---
+++ Study_React/Study_React_1027.md
@@ -0,0 +1,22 @@
+# React 10 27
+
+## components
+**components(구성 요소)** , React를 구성하는 요소가 components이다.
+components는 자바스크립트의 function과 유사하다.
+
+```
+React component의 입력은 Props로 받은 후 React element(가장 작은 빌딩 블록)로 출력하게 된다.
+
+props(속성) -> component의 속성
+props는 Read-only(읽기 전용)
+```
+
+```
+component(이름이 항상 대문자여야 한다. 소문자면 리액트 돔으로 인식)
+
+1. Function Component
+
+2. Class Component
+
+component안에 또 다른 component를 넣어 합성 가능
+```
--- Study_React/my-app/src/index.js
+++ Study_React/my-app/src/index.js
@@ -4,36 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const formatName = function(name){
-  return name.firstName+ " " + name.lastName;
-}
-const name = {
-  firstName: "Jinwoo",
-  lastName: "Lee",
-};
-const element = <h1>Hello, {formatName(name)}</h1>
-// element는 주로 화면에 보이는 것들을 기술함
-// element 기술 후에는 children 혹은 attribute를 바꿀 수 없음
-ReactDOM.render(
-  element,
-  document.getElementById('root'));
-
-const element1 = <h1>Hello, world!</h1>
-ReactDOM.render(element1,
-  document.getElementById('root'));
-
-function tick(){
-  const element2 = (
+function tick() {
+  const element = (
     <div>
       <h1>Hello, world!</h1>
-  <h2>It is {new Date().toLocaleString()}</h2>
+  <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
-  ReactDOM.render(element2,
+  ReactDOM.render(
+    element,
     document.getElementById('root'));
 }
 setInterval(tick, 1000);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
